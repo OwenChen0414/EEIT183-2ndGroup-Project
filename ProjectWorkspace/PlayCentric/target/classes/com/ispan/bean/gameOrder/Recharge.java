f4b0c8a3c9c168882fd19e83e96645379d7b847a
TS8582 <a0958979918@gmail.com>
2024-06-19 08:36:04 +0800

    update one table name

--- ProjectWorkspace/PlayCentric/target/classes/com/ispan/bean/gameOrder/Recharge.java
+++ ProjectWorkspace/PlayCentric/target/classes/com/ispan/bean/gameOrder/Recharge.java
@@ -10,7 +10,7 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 
-@Entity @Table(name = "addPoint")
+@Entity @Table(name = "recharge")
 public class Recharge {
 	
 	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
